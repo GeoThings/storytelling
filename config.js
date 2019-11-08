77260f84d68956836c4355a004915ed79d6bafde
typebrook <typebrook@gmail.com>
2019-11-08 18:53:22 +0800

    Put index.html at root

---
+++ config.js
@@ -0,0 +1,100 @@
+var config = {
+    style: 'mapbox://styles/mapbox/streets-v11',
+    accessToken: 'pk.eyJ1IjoidHlwZWJyb29rIiwiYSI6ImNqNHVyaTc5dDBuazczMm1jenl3cG8wb3IifQ.2UEZ-jiHgHvYYqVirXhgpw',
+    showMarkers: true,
+    theme: 'dark',
+    alignment: 'right',
+    title: 'geoBingAn filter',
+    subtitle: 'A bunch of reports',
+    byline: 'By a Digital Storyteller',
+    footer: 'Source: source citations, etc.',
+    chapters:
+
+[
+  {
+    "id": "2bc3277db9194a1c95ae0f9bda2bb226",
+    "title": "避難處所聯絡資訊",
+    "image": "https://uploads.geobingan.info/attachment/f1762529957d4493aa68ae5dc5d0f5d1.jpg",
+    "description": "尖石鄉嘉樂村疏散避難圖\n村長 呂金龍/0928500055",
+    "location": {
+      "center": [
+        121.208573149643,
+        24.7067191711656
+      ],
+      "zoom": 17,
+      "pitch": 60,
+      "bearing": 0
+    },
+    "onChapterEnter": [],
+    "onChapterExit": []
+  },
+  {
+    "id": "4588ef1fb7f641a5876806fef1601a0f",
+    "title": "避難處所聯絡資訊",
+    "image": "https://uploads.geobingan.info/attachment/7a27a6447786465cbf93afa7dae443f6.jpg",
+    "description": "尖石鄉嘉樂村疏散避難圖\n村長 呂金龍/0928500055",
+    "location": {
+      "center": [
+        121.210898475886,
+        24.7054482012512
+      ],
+      "zoom": 17,
+      "pitch": 60,
+      "bearing": 0
+    },
+    "onChapterEnter": [],
+    "onChapterExit": []
+  },
+  {
+    "id": "0e52b32f0c0343459c7b5ef255abc906",
+    "title": "避難處所聯絡資訊",
+    "image": "https://uploads.geobingan.info/attachment/ae071314116945f18d3355a4950a30ea.png",
+    "description": "新竹市東區【千甲里】簡易疏散避難圖\n緊急聯絡人 里長：曾資強 電話：0932-284560\n撥打 1991 平安留言平台",
+    "location": {
+      "center": [
+        121.002439787397,
+        24.812405287282
+      ],
+      "zoom": 17,
+      "pitch": 60,
+      "bearing": 0
+    },
+    "onChapterEnter": [],
+    "onChapterExit": []
+  },
+  {
+    "id": "04dad5d902694079bc879a343dfd83ef",
+    "title": "避難處所聯絡資訊",
+    "image": "https://uploads.geobingan.info/attachment/f78d6e21232e48e0a1fd8f7bf68812ea.jpg",
+    "description": "尖石鄉嘉樂村疏散避難圖\n村長 呂金龍/0928500055",
+    "location": {
+      "center": [
+        121.226836440844,
+        24.7280670798442
+      ],
+      "zoom": 17,
+      "pitch": 60,
+      "bearing": 0
+    },
+    "onChapterEnter": [],
+    "onChapterExit": []
+  },
+  {
+    "id": "48db8595873e4c1db890bba65f372bdc",
+    "title": "避難處所聯絡資訊",
+    "image": "https://uploads.geobingan.info/attachment/b6d7e217f3944e5a96f3f958a0e9258b.png",
+    "description": "新竹市東區【千甲里】簡易疏散避難圖\n緊急聯絡人 里長：曾資強 電話：0932-284560\n撥打 1991 平安留言平台",
+    "location": {
+      "center": [
+        121.000880091047,
+        24.7973494386194
+      ],
+      "zoom": 17,
+      "pitch": 60,
+      "bearing": 0
+    },
+    "onChapterEnter": [],
+    "onChapterExit": []
+  }
+]
+};
